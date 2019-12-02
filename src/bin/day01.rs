92c1ca8046a54f5db348041e1db80a0e13e271c9
Chris Tompkinson <chris.tompkinson@xtxmarkets.com>
2019-12-02 13:13:02 +0000

    Solution for days 1, and 2

---
+++ src/bin/day01.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn total_fuel(a: i32) -> i32 {
+    let fuel = a/3 - 2;
+    if fuel <= 0 {
+        return 0;
+    }
+    return fuel + total_fuel(fuel);
+}
+
+fn main() {
+    let file = File::open("input/day01.in").expect("Failed to open input");
+    let file = BufReader::new(&file);
+
+    let masses: Vec<i32> = file.lines()
+        .map(|x| x.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    let fuel: i32 = masses.iter().map(|x| x/3-2).sum();
+    println!("Part 1: {}", fuel);
+    let tot: i32 = masses.iter().map(|x| total_fuel(*x)).sum();
+    println!("Part 2: {}", tot);
+}
